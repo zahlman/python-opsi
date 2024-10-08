202c1c82e6dc5220901799a6fdcdee50b0609bcf
Jan Schneider <j.schneider@uib.de>
2024-10-08 12:09:06 +0200

    [fix] Fix get_subprocess_environment

--- OPSI/System/Posix.py
+++ OPSI/System/Posix.py
@@ -912,33 +912,11 @@ def which(cmd: str, env: dict = None) -> str:
 def get_subprocess_environment(
 	env: dict = None, add_lc_all_C=False, add_path_sbin=False
 ):
-	sp_env = env
-	if sp_env is None:
-		sp_env = os.environ.copy()
-	if getattr(sys, "frozen", False):
-		# Running in pyinstaller / frozen
-		lp_orig = sp_env.get("LD_LIBRARY_PATH_ORIG")
-		if lp_orig is not None:
-			lp_orig = os.pathsep.join(
-				[
-					entry
-					for entry in lp_orig.split(os.pathsep)
-					if entry not in LD_LIBRARY_EXCLUDE_LIST
-				]
-			)
-			# Restore the original, unmodified value
-			logger.debug(
-				"Setting original LD_LIBRARY_PATH '%s' in env for subprocess", lp_orig
-			)
-			sp_env["LD_LIBRARY_PATH"] = lp_orig
-		else:
-			# This happens when LD_LIBRARY_PATH was not set.
-			# Remove the env var as a last resort
-			logger.debug("Removing LD_LIBRARY_PATH from env for subprocess")
-			sp_env.pop("LD_LIBRARY_PATH", None)
+	sp_env = opsicommon_get_subprocess_environment(env)
 
 	if add_lc_all_C:
 		sp_env["LC_ALL"] = "C"
+
 	if add_path_sbin:
 		path_parts = sp_env["PATH"].split(":")
 		if "/usr/local/sbin" not in path_parts:
